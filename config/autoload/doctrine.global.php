7654de55671283a29ea225b89a50c3fc55fe70bb
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2022-01-07 16:24:48 +0000

    cleanup and pdoAuth readme

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -1,16 +1,7 @@
 <?php
-//use Doctrine\ORM\Mapping\UnderscoreNamingStrategy;
 use Doctrine\DBAL\Driver\PDO\PgSQL\Driver;
 return [
     'doctrine' => [
-        'configuration' => [
-            'orm_default' => [
-                'auto_generate_proxy_classes' => false,
-                'proxy_dir'                   => 'data/proxies',
-                'sql_logger' => 'my_sql_logger',
-                //'naming_strategy'             => UnderscoreNamingStrategy::class,
-            ],
-        ],
         'connection' => [
             'orm_default' => [
                 'driver_class' => Driver::class,
@@ -20,14 +11,7 @@ return [
                     'options' => [
                         1002 => 'SET NAMES utf8',
                     ],
-                    'serverVersion' => '10.5.12-MariaDB', // setting this lets the CLI work without a live DB connection (e.g. in CI)
-                ],
-            ],
-        ],
-        'migrations' => [
-            'orm_default' => [
-                'migrations_paths' => [
-                    'Carlease' => 'data/migrations',
+                    //'serverVersion' => '10.5.12-MariaDB', // setting this lets the CLI work without a live DB connection (e.g. in CI)
                 ],
             ],
         ],
