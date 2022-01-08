383c7f0ee30b68026196cf631f9e16890c1f7de6
Ganapathi Chidambaram <ganapathi.rj@gmail.com>
2022-01-08 23:55:32 +0000

    template changed to adminlte

--- src/App/templates/js/technobureau.js
+++ src/App/templates/js/technobureau.js
@@ -1,26 +1,6 @@
 require('./bootstrap');
 require('./bootstrap-select');
 
-(function($) {
-    "use strict";
-
-    // Add active state to sidbar nav links
-    var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
-        $("#layoutSidenav_nav .sb-sidenav a.nav-link").each(function() {
-            if (this.href === path) {
-                $(this).addClass("active");
-            }
-        });
-
-    // Toggle the side navigation
-    $("#sidebarToggle").on("click", function(e) {
-        e.preventDefault();
-        $("body").toggleClass("sb-sidenav-toggled");
-    });
-})(jQuery);
-$(document).on('show.bs.modal','#confirmation-modal', function (event){
-    $(this).find('#name').text($(event.relatedTarget).data('bs-name'))
-    $('#confirmationDelForm').attr('action', $(event.relatedTarget).data('bs-attr'));
-    $(this).scrollTop(0);
-
-})
+try {
+  require('tb-admin-lte');
+} catch (e) {}
